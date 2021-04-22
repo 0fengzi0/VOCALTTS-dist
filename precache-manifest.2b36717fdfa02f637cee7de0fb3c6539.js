2d43b381828f8ff3ab39fba52a062f88cf0b6e04
GitHub Action <Action@GitHub.com>
2021-04-22 12:39:53 +0800

    GitHub Actions Auto Builder at 2021-04-22 12:39:53

---
+++ precache-manifest.2b36717fdfa02f637cee7de0fb3c6539.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72941a3fb75426aaf2e681c853e634b",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/404.html"
+  },
+  {
+    "revision": "fbfbdc6ceff279d071f00d22ae0043f5",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/CNAME"
+  },
+  {
+    "revision": "d87063e340bff1a6dc8a",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/css/app.c3e1fe33.css"
+  },
+  {
+    "revision": "d1ed918c561446e16a94",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/css/chunk-1a571c50.e0b28688.css"
+  },
+  {
+    "revision": "f6b2c3a3374798c6f084",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/css/chunk-4b9f742c.43f20126.css"
+  },
+  {
+    "revision": "428bff88602c30548ff1",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/css/chunk-50a805ee.01b04682.css"
+  },
+  {
+    "revision": "587564368d9a6733b012",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/css/chunk-5ec774f7.a5b6e0ba.css"
+  },
+  {
+    "revision": "eb70ffe27ca8d5473a17",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/css/chunk-6b122f8c.fd895bf4.css"
+  },
+  {
+    "revision": "75fcd49aff5c537c7611",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/css/chunk-a1d4cdf4.9179c317.css"
+  },
+  {
+    "revision": "238e60fac9f20e874052",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/css/chunk-c5727314.20aebb10.css"
+  },
+  {
+    "revision": "f46f892194bf5a4e7f95",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/css/chunk-vendors.f3ebfab6.css"
+  },
+  {
+    "revision": "c540b6e58c1c30ee1add06b2c42bbbf9",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/index.html"
+  },
+  {
+    "revision": "d87063e340bff1a6dc8a",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/app.57766e1f.js"
+  },
+  {
+    "revision": "d1ed918c561446e16a94",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-1a571c50.c5ccdbee.js"
+  },
+  {
+    "revision": "19c462433d5939f15671",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-216031a0.2493860a.js"
+  },
+  {
+    "revision": "f6b2c3a3374798c6f084",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-4b9f742c.a721db8a.js"
+  },
+  {
+    "revision": "428bff88602c30548ff1",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-50a805ee.e0c77fc5.js"
+  },
+  {
+    "revision": "587564368d9a6733b012",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-5ec774f7.19443a0d.js"
+  },
+  {
+    "revision": "eb70ffe27ca8d5473a17",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-6b122f8c.0d110234.js"
+  },
+  {
+    "revision": "75fcd49aff5c537c7611",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-a1d4cdf4.35351fe1.js"
+  },
+  {
+    "revision": "238e60fac9f20e874052",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-c5727314.760cb5da.js"
+  },
+  {
+    "revision": "f46f892194bf5a4e7f95",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-vendors.ba23bd36.js"
+  },
+  {
+    "revision": "8ff17bdee0e1a03e3665ca28b12ea002",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-vendors.ba23bd36.js.LICENSE.txt"
+  },
+  {
+    "revision": "25e5021e3a5fd3836a4c446bb1f3ab65",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/manifest.json"
+  },
+  {
+    "revision": "a38ee283089c8ac84c4a7f2dcc0d9ed2",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/robots.txt"
+  }
+]);
