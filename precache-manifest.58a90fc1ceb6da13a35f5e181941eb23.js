c51c025e7f4a43167167e00113a8748510bd5719
GitHub Action <Action@GitHub.com>
2022-09-11 17:22:55 +0800

    GitHub Actions Auto Builder at 2022-09-11 17:22:55

---
+++ precache-manifest.58a90fc1ceb6da13a35f5e181941eb23.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72941a3fb75426aaf2e681c853e634b",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/404.html"
+  },
+  {
+    "revision": "fbfbdc6ceff279d071f00d22ae0043f5",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/CNAME"
+  },
+  {
+    "revision": "56ed202f4861c6c78e41",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/css/app.c3e1fe33.css"
+  },
+  {
+    "revision": "88fc24b7576696b5de1b",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/css/chunk-28909d66.eec978dc.css"
+  },
+  {
+    "revision": "00bddcbf74fb2d99c7a6",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/css/chunk-2f00a745.4243beeb.css"
+  },
+  {
+    "revision": "cce0bb6d761266304933",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/css/chunk-34b5fb7c.536d6143.css"
+  },
+  {
+    "revision": "f94b21ab0e40d6919b2f",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/css/chunk-3511caa1.e5f1ebc0.css"
+  },
+  {
+    "revision": "b6d78550757ca7bd1e73",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/css/chunk-819c36e8.0ffd629b.css"
+  },
+  {
+    "revision": "11bfa24ee1f06a44f276",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/css/chunk-acca98e0.5ffb12ff.css"
+  },
+  {
+    "revision": "093b67110c72a5ad913e",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/css/chunk-b77c1e0e.7068c915.css"
+  },
+  {
+    "revision": "060146ad001a63768b0b",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/css/chunk-vendors.5bbfe247.css"
+  },
+  {
+    "revision": "3ebb3b835f25b454f3866f9998444fae",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/index.html"
+  },
+  {
+    "revision": "56ed202f4861c6c78e41",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/app.ae708b22.js"
+  },
+  {
+    "revision": "b6be653a77fa30c75d17",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-190d601e.a329a875.js"
+  },
+  {
+    "revision": "88fc24b7576696b5de1b",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-28909d66.7c52d4dd.js"
+  },
+  {
+    "revision": "00bddcbf74fb2d99c7a6",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-2f00a745.9461c08a.js"
+  },
+  {
+    "revision": "cce0bb6d761266304933",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-34b5fb7c.94e8cd4e.js"
+  },
+  {
+    "revision": "f94b21ab0e40d6919b2f",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-3511caa1.11deaefb.js"
+  },
+  {
+    "revision": "b6d78550757ca7bd1e73",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-819c36e8.b8ce7cf6.js"
+  },
+  {
+    "revision": "7f2b5dae6d75c7e33176",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-96de4db2.23cbbfb6.js"
+  },
+  {
+    "revision": "11bfa24ee1f06a44f276",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-acca98e0.9dec22a1.js"
+  },
+  {
+    "revision": "093b67110c72a5ad913e",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-b77c1e0e.448e8c22.js"
+  },
+  {
+    "revision": "060146ad001a63768b0b",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-vendors.f8e76378.js"
+  },
+  {
+    "revision": "8a3d82afa99adfd60b5234d07429b479",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/js/chunk-vendors.f8e76378.js.LICENSE.txt"
+  },
+  {
+    "revision": "25e5021e3a5fd3836a4c446bb1f3ab65",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/manifest.json"
+  },
+  {
+    "revision": "a38ee283089c8ac84c4a7f2dcc0d9ed2",
+    "url": "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://cdn.jsdelivr.net/gh/0fengzi0/VOCALTTS-dist/precache-manifest.58a90fc1ceb6da13a35f5e181941eb23.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "vocaltts"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
